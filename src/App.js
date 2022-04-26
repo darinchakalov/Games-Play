4bb87439281b7661bbc7eec09a6a3faf12ebb796
darinchakalov <darinchakalov@hotmail.com>
2022-04-27 02:16:47 +0300

    created route handler and routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
+import { createElement, useState } from "react";
+
 import Header from "./components/Header.js";
 import WelcomeWorld from "./components/WelcomeWorld.js";
+import GameCatalog from "./components/GameCatalog.js";
+import CreateGame from "./components/CreateGame.js";
 
 function App() {
+	const [page, setPage] = useState("/home");
+
+	const routes = {
+		"/home": WelcomeWorld,
+		"/games": GameCatalog,
+		"/create-game": CreateGame,
+	};
+
+	const navigationChangeHandler = (path) => {
+		setPage(path);
+	};
+
 	return (
 		<div id="box">
-			<Header />
+			<Header navigationChangeHandler={navigationChangeHandler} />
 
-			<main id="main-content"></main>
-
-			<WelcomeWorld />
-
-			
-
-			
-
-			
-
-			
-
-			
-
-			
+			<main id="main-content">{createElement(routes[page]) || <h2>Page not found </h2>}</main>
 		</div>
 	);
 }
