8ffbcec92850bfc40b316e09cbfc0b29eed7756b
darinchakalov <darinchakalov@hotmail.com>
2022-05-01 17:57:50 +0300

    implemented link instead of a href

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
 			<main id="main-content">
 				<Switch>
 					<Route path="/" exact component={WelcomeWorld} />
-					<Route path="/games" component={GameCatalog} />
+					<Route path="/games" exact component={GameCatalog} />
 					<Route path="/create-game" component={CreateGame} />
 					<Route path="/edit-game" component={EditGame} />
 					<Route path="/register" component={Register} />
